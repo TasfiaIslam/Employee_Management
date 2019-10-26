1e2f2fcf7070556657d43d3aee56859c430c6485
Tasfia Islam <tasf1a027@gmail.com>
2019-10-26 21:05:23 +0600

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Employee_Management.Models;
+
+namespace Employee_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        // Inject IEmployeeRepository using Constructor Injection
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        // Retrieve employee name and return
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Management.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id=1,Name="Tasfia",Department="Technology",Email="tasfia@mail.com"},
+                new Employee(){Id=2,Name="Adeeb",Department="Technology",Email="adeeb@mail.com"},
+                new Employee(){Id=3,Name="Nabila",Department="Technology",Email="nabila@mail.com"}
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return this._employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
